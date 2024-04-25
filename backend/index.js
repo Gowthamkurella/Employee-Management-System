a6f81f219fcabfe189c45cd31589b41f5f073a79
K.N.V.GOWTHAM <110026459+Gowthamkurella@users.noreply.github.com>
2024-04-25 23:46:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,18 +13,14 @@ const emp = require('./models/t_employee');
 const AWS = require("aws-sdk");
 const multer = require("multer");
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://employee-management-system-lr1k.onrender.com/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+const corsOptions = {
+  origin: 'https://employee-management-system-lr1k.onrender.com',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
 
-
-app.use(cors({
-  origin: 'https://employee-management-system-lr1k.onrender.com/',
-  credentials: true
-}));
 
 
 
