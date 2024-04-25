aa9e4f5bf41404af254f9856eee4872e2066cb4e
K.N.V.GOWTHAM <110026459+Gowthamkurella@users.noreply.github.com>
2024-04-25 23:38:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const AWS = require("aws-sdk");
 const multer = require("multer");
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://employee-management-system-lr1k.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://employee-management-system-lr1k.onrender.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 
 app.use(cors({
-  origin: 'https://employee-management-system-lr1k.onrender.com',
+  origin: 'https://employee-management-system-lr1k.onrender.com/',
   credentials: true
 }));
 
