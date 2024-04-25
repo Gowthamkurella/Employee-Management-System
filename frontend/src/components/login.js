78ba4693d0483d77605d9f6d356ca1cb29bbe184
K.N.V.GOWTHAM <110026459+Gowthamkurella@users.noreply.github.com>
2024-04-25 22:38:33 +0530

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -19,7 +19,7 @@ function Login() {
       }
   
       try {
-        const response = await fetch('http://localhost:5000/login', {
+        const response = await fetch('https://employee-management-system-three-kappa.vercel.app/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ username, password }),
